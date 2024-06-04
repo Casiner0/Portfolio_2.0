7042fd2e5cf4a3559267a8eec56ff8e216ea484f
Howard <howard@softel.cu>
2024-06-04 09:39:09 +0100

    agregado menu responsive

--- js/funtions.js
+++ js/funtions.js
@@ -13,47 +13,14 @@ $(function () {
         swichtMode();
     })
 
-    // Animación de las skills
-    let skillsSection = false;
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                skillsEffect();
-            }
-        });
-    });
-    
-    const skills = document.getElementById("skills");
-    observer.observe(skills);
-    
-    const skillsEffect = () => {
-        if (!skillsSection) {
-            skillsSection = true;
-            setTimeout(() => {
-                let hability = document.getElementsByClassName("progress");
-                hability[0].classList.add("javascript");
-                hability[1].classList.add("php");
-                hability[2].classList.add("htmlcss");
-                hability[3].classList.add("java");
-                hability[4].classList.add("sql");
-                hability[5].classList.add("odoo15");
-                hability[6].classList.add("react");
-                hability[7].classList.add("photoshop");
-                hability[8].classList.add("max-studio");
-                hability[9].classList.add("c-logos");
-                hability[10].classList.add("e-video");
-            }, 500);
-        }
-    };
 
     const swichtMode = () => {
         if($('#dark-mode').is(':checked')) {
-            $('.cont-banner, .container-header, #navLink, .net-circle, .section-dark').addClass('ligth'); 
-            $('.cont-banner, .container-header, #navLink, .net-circle, .section-dark').removeClass('dark');
+            $('.cont-banner, .cont-nav-responsive, .container-header, #navLink, .net-circle, .section-dark').addClass('ligth'); 
+            $('.cont-banner, .cont-nav-responsive, .container-header, #navLink, .net-circle, .section-dark').removeClass('dark');
             $('.section-less-dark, .interesting').addClass('more-ligth'); 
             $('.net-circle').css('border', '1px solid #252a2e')
-            $('.home, .cv').css('background', 'linear-gradient(to top, rgb(222, 221, 221, .8), rgb(195, 195, 195, 1))')
+            $('.home, .cv').css('background', 'linear-gradient(to top, rgb(222, 221, 221, .), rgb(195, 195, 195, 1))')
             $('.contacts').css({
                 'background': ['linear-gradient(to top, rgba(255, 255, 255, .8), rgba(195, 195, 195, 1))','url("./img/cube-dark-texture.jpg")'],
                 'background-size': 'cover',
@@ -68,8 +35,8 @@ $(function () {
             $('.anim-button').css({'border': '2px solid #1e2326','color':'#1e2326'})
             $('.form-input').css({'background-color':'#dedddd'})
         } else {
-            $('.cont-banner, .container-header, #navLink, .net-circle, .section-dark').addClass('dark');
-            $('.cont-banner, .container-header, #navLink, .net-circle, .section-dark').removeClass('ligth');
+            $('.cont-banner, .cont-nav-responsive, .container-header, #navLink, .net-circle, .section-dark').addClass('dark');
+            $('.cont-banner, .cont-nav-responsive, .container-header, #navLink, .net-circle, .section-dark').removeClass('ligth');
             $('.net-circle').css('border', '1px solid #d9d9d9')
             $('.section-less-dark, .interesting').removeClass('more-ligth');
             $('.home, .cv').css('background', 'linear-gradient(to top, rgba(30,35,38,.8), rgba(30,35,38,1))')
@@ -91,3 +58,21 @@ $(function () {
     
     
 });
+
+//Mostrar u ocultar menu
+let menuVible = false;
+
+const showHideMenu = () => {
+    if (menuVible) {
+        select()
+    } else {
+        $('#nav').attr('class', 'responsive');
+        menuVible = true;
+    }
+}
+
+//Oculta el menu despues de seleccionar
+const select = () => {
+    $('#nav').attr('class', '');
+        menuVible = false;
+}
